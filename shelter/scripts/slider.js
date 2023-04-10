a12ac88b172f41404bfecc6cf71916afdd4cb694
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-04-10 15:12:42 +0300

    feat: almost working slider, apart randomness

--- shelter/scripts/slider.js
+++ shelter/scripts/slider.js
@@ -4,12 +4,14 @@ const Slider = new class {
     constructor() {
         const slider = document.querySelector('.slider')
 
-        const firstSlide = this.createSlide(0, 1, 2)
+        const initialSet = getRandomNum(8, 3)
+        this.currentSet = initialSet
+        const firstSlide = this.createSlide(initialSet)
         this.rowElem = slider.querySelector('.slider__row')
         this.rowElem.append(
-            this.createSlide(3, 5, 6), 
+            this.createSlide(getRandomNum(8, 3, initialSet)), 
             firstSlide,
-            this.createSlide(4, 3, 7)
+            this.createSlide(getRandomNum(8, 3, initialSet))
         )
 
         const buttons = slider.querySelectorAll('[data-direction]')
@@ -29,40 +31,55 @@ const Slider = new class {
             + сдвиг на 0
             + без анимации вставить новый первый слайд и вернуть на -100
          */
-
-        const direction = event.target.dataset.direction
+        const button = event.target.closest('[data-direction]')
+        const direction = button.dataset.direction
         if (direction === 'right') {
             this.rowElem.classList.add('slider__row_slide-right')
-            this.rowElem.addEventListener('transitionend', deleteFirstAddThird.bind(this))
+            setTimeout(deleteFirstAddThird.bind(this), 700)
+            // this.rowElem.addEventListener('transitionend', deleteFirstAddThird.bind(this))
         } else {
             this.rowElem.classList.add('slider__row_slide-left')
-            this.rowElem.addEventListener('transitionend', addFirstDeleteThird.bind(this))
+            setTimeout(addFirstDeleteThird.bind(this), 700)
+            // this.rowElem.addEventListener('transitionend', addFirstDeleteThird.bind(this))
         }
 
         function deleteFirstAddThird() {
             this.rowElem.classList.remove('slider__row_slide-right')
             this.rowElem.firstElementChild.remove()
-            this.rowElem.append(this.createSlide(0, 0, 0))
+            this.rowElem.append(this.getNewSlide())
         }
 
         function addFirstDeleteThird() {
             this.rowElem.classList.remove('slider__row_slide-left')
-            this.rowElem.prepend(this.createSlide(1, 1, 1))
+            this.rowElem.prepend(this.getNewSlide())
             this.rowElem.lastElementChild.remove()
         }
     }
 
-    
-
-    getNewCards() {
-
+    getNewSlide() {
+        const newSet = getRandomNum(8, 3, this.currentSet || [])
+        this.currentSet = newSet
+        return this.createSlide(newSet)
     }
 
-    createSlide(...cardNums) {
+    createSlide(cardNums) {
         const slide = document.createElement('div')
         slide.classList.add('slider__slide')
-        slide.append(...Pets.getCards(...cardNums))
+        slide.append(...Pets.getCards(cardNums))
         return slide
     }
 }
 
+function getRandomNum(range, take = 1, apart = []) {
+    let source = Array.from(Array(range).keys())
+    source = source.filter(el => !apart.includes(el))
+    
+    let result = []
+    while (result.length < take && source.length > 0) {
+        const index = Math.floor(Math.random() * source.length)
+        result.push(source[index])
+        source.splice(index, 1)
+    }
+    return result
+}
+// console.log(getRandomNum(range = 9))
