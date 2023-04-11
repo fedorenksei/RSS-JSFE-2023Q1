34a896e0ad568846bbf6b34d6517a5bbd9bdab95
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-04-11 11:17:50 +0300

    Revert "feat: make sliding"
    This reverts commit d271d245dc6927fbf1c6643a2f61862368172ac1.

--- shelter/scripts/pets.js
+++ shelter/scripts/pets.js
@@ -37,9 +37,6 @@ HTML + CSS + JS
 import petsData from './pets-data.json' assert {type: 'json'}
 
 export const Pets = new class {
-    getAmount() {
-        return petsData.length
-    }
     getCard(petNum) {
         const petData = petsData[petNum]
         const petName = petData.name
--- shelter/scripts/slider.js
+++ shelter/scripts/slider.js
@@ -3,66 +3,13 @@ import {Pets} from './pets.js'
 const Slider = new class {
     constructor() {
         const slider = document.querySelector('.slider')
-
-        const firstSlide = this.createSlide(0, 1, 2)
         this.rowElem = slider.querySelector('.slider__row')
-        this.rowElem.append(
-            this.createSlide(3, 5, 6), 
-            firstSlide,
-            this.createSlide(4, 3, 7)
-        )
 
-        const buttons = slider.querySelectorAll('[data-direction]')
-        for (const button of buttons) {
-            button.addEventListener('click', this.buttonListener.bind(this))
-        }
-    }
-
-    buttonListener(event) {
-        /* 
-        три слайда (сдвинуто на -100)
-        - кнопка вправо:
-            + сдвиг на -200
-            + без анимации убрать первый слайд и вернуть на -100
-            + добавить новый третий слайд
-        - кнопка влево
-            + сдвиг на 0
-            + без анимации вставить новый первый слайд и вернуть на -100
-         */
-
-        const direction = event.target.dataset.direction
-        if (direction === 'right') {
-            this.rowElem.classList.add('slider__row_slide-right')
-            setTimeout(deleteFirstAddThird.bind(this), 700)
-        } else {
-            this.rowElem.classList.add('slider__row_slide-left')
-            setTimeout(addFirstDeleteThird.bind(this), 700)
-        }
-
-        function deleteFirstAddThird() {
-            this.rowElem.classList.remove('slider__row_slide-right')
-            this.rowElem.firstElementChild.remove()
-            this.rowElem.append(this.createSlide(0, 0, 0))
-        }
-
-        function addFirstDeleteThird() {
-            this.rowElem.classList.remove('slider__row_slide-left')
-            this.rowElem.prepend(this.createSlide(1, 1, 1))
-            this.rowElem.lastElementChild.remove()
-        }
-    }
-
-    
-
-    getNewCards() {
-
-    }
-
-    createSlide(...cardNums) {
         const slide = document.createElement('div')
         slide.classList.add('slider__slide')
-        slide.append(...Pets.getCards(...cardNums))
-        return slide
+        slide.append(...Pets.getCards(5, 1, 2))
+
+        this.rowElem.appendChild(slide)
     }
 }
 
