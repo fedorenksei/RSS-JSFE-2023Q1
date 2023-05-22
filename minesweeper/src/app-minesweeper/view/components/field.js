7cf2dc7655d9641f30ec2a84a5bdf33debbf7b21
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-05-22 10:32:43 +0300

    refactor(view): separate logic and view in field

--- minesweeper/src/app-minesweeper/view/components/field.js
+++ minesweeper/src/app-minesweeper/view/components/field.js
@@ -1,21 +1,20 @@
-import './field.css';
-import createElement from '../createElement';
+import * as fieldView from './fieldView';
 import * as cellFactory from './cell';
 
-const MAX_CELLS = 25 ** 2;
-const CLASSES = {
-  fieldInit: 'minesweeper__field minesweeper-field',
-  fieldSizes: {
-    small: 'minesweeper-field_size_small',
-    medium: 'minesweeper-field_size_medium',
-    large: 'minesweeper-field_size_large',
-  },
-  disabled: 'minesweeper-field_disabled',
-};
-
 let modelApi;
-const fieldElement = createElement('div', CLASSES.fieldInit);
-const cellById = new Map();
+let cellById;
+export function init(api) {
+  modelApi = api;
+  cellFactory.init({ modelApi, fieldApi });
+
+  fieldView.init(modelApi);
+  cellById = fieldView.getCellsMap();
+
+  if (!modelApi.currentGame) {
+    fieldView.setSize('small');
+  }
+}
+
 const flaggedCells = new Set();
 const modifiedCells = new Set();
 const fieldApi = {
@@ -28,38 +27,6 @@ const fieldApi = {
     modifiedCells.delete(id);
   },
 };
-
-export function init(api) {
-  modelApi = api;
-  cellFactory.init({ modelApi, fieldApi });
-
-  createAllCells();
-
-  if (!modelApi.currentGame) {
-    setSize('small');
-  }
-}
-
-function createAllCells() {
-  let currentId = 0;
-  const cellElements = [];
-  for (let index = 0; index < MAX_CELLS; index += 1, currentId += 1) {
-    const cell = cellFactory.create(currentId);
-    cellById.set(currentId, cell);
-    cellElements.push(cell.getElement());
-  }
-  fieldElement.replaceChildren(...cellElements);
-}
-
-function setSize(size) {
-  Object.entries(CLASSES.fieldSizes).forEach(([key, value]) => {
-    if (key === size) {
-      fieldElement.classList.add(value);
-    } else {
-      fieldElement.classList.remove(value);
-    }
-  });
-}
 
 export function revealCell({ primaryCell, secondaryCells }) {
   reveal(primaryCell);
@@ -90,13 +57,13 @@ export function revealField({ explodedId, minesIds }) {
     }
   });
 
-  fieldElement.classList.add(CLASSES.disabled);
+  fieldView.disableField();
 }
 
 export function winGame() {
-  fieldElement.classList.add(CLASSES.disabled);
+  fieldView.disableField();
 }
 
 export function getElement() {
-  return fieldElement;
+  return fieldView.getElement();
 }
---
+++ minesweeper/src/app-minesweeper/view/components/fieldView.js
@@ -0,0 +1,59 @@
+import './field.css';
+import createElement from '../createElement';
+import * as cellFactory from './cell';
+
+let modelApi;
+export function init(api) {
+  modelApi = api;
+  createAllCells();
+
+  if (!modelApi.currentGame) {
+    setSize('small');
+  }
+}
+
+const MAX_CELLS = 25 ** 2;
+const CLASSES = {
+  fieldInit: 'minesweeper__field minesweeper-field',
+  fieldSizes: {
+    small: 'minesweeper-field_size_small',
+    medium: 'minesweeper-field_size_medium',
+    large: 'minesweeper-field_size_large',
+  },
+  disabled: 'minesweeper-field_disabled',
+};
+const fieldElement = createElement('div', CLASSES.fieldInit);
+
+const cellById = new Map();
+export function getCellsMap() {
+  return cellById;
+}
+
+function createAllCells() {
+  let currentId = 0;
+  const cellElements = [];
+  for (let index = 0; index < MAX_CELLS; index += 1, currentId += 1) {
+    const cell = cellFactory.create(currentId);
+    cellById.set(currentId, cell);
+    cellElements.push(cell.getElement());
+  }
+  fieldElement.replaceChildren(...cellElements);
+}
+
+export function setSize(size) {
+  Object.entries(CLASSES.fieldSizes).forEach(([key, value]) => {
+    if (key === size) {
+      fieldElement.classList.add(value);
+    } else {
+      fieldElement.classList.remove(value);
+    }
+  });
+}
+
+export function disableField() {
+  fieldElement.classList.add(CLASSES.disabled);
+}
+
+export function getElement() {
+  return fieldElement;
+}
