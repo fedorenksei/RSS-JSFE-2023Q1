e36bd044c26dde33d6943d0c7862166f7a9baedf
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-06-11 17:30:48 +0300

    refactor: apply 2 space indentation

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '062ee587191f42918ec1daaea19f2827', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '062ee587191f42918ec1daaea19f2827', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
