71f044fe0299fef2f676a96a502fae236a5e37a9
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-06-12 23:38:24 +0300

    refactor: migrate appView.js to TS

--- migration-to-ts/src/components/view/appView.ts
+++ migration-to-ts/src/components/view/appView.ts
@@ -1,18 +1,22 @@
+import { Article, Source } from '../types';
 import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+  private news: News;
+  private sources: Sources;
+
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  drawNews(data) {
+  public drawNews(data: { articles: Article[] }) {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data) {
+  public drawSources(data: { sources: Source[] }) {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
