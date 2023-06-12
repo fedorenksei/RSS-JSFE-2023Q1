1a426ae852e850a93b0aa9817f222c9ba6877342
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-06-12 17:12:44 +0300

    refactor: migrate loader.js to TS

--- migration-to-ts/src/components/controller/loader.ts
+++ migration-to-ts/src/components/controller/loader.ts
@@ -1,11 +1,33 @@
+type LoaderOptions = {
+  apiKey: string;
+};
+
+type NewsApiOptions = {
+  sources?: string;
+};
+
+type NewsApiEndpoint = 'sources' | 'everything';
+
+type FetchResponse = {
+  ok: boolean;
+  status: number;
+  statusText: string;
+  json: () => Promise<JSON>;
+};
+
+type CallbackFromView = (data?: JSON) => void;
+
 class Loader {
-  constructor(baseLink, options) {
+  baseLink: string;
+  options: LoaderOptions;
+
+  constructor(baseLink: string, options: LoaderOptions) {
     this.baseLink = baseLink;
     this.options = options;
   }
 
   getResp(
-    { endpoint, options = {} },
+    { endpoint, options = {} }: { endpoint: NewsApiEndpoint; options: NewsApiOptions },
     callback = () => {
       console.error('No callback for GET response');
     }
@@ -13,7 +35,7 @@ class Loader {
     this.load('GET', endpoint, callback, options);
   }
 
-  errorHandler(res) {
+  errorHandler(res: FetchResponse) {
     if (!res.ok) {
       if (res.status === 401 || res.status === 404)
         console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +45,21 @@ class Loader {
     return res;
   }
 
-  makeUrl(options, endpoint) {
+  makeUrl(options: NewsApiOptions, endpoint: NewsApiEndpoint) {
     const urlOptions = { ...this.options, ...options };
     let url = `${this.baseLink}${endpoint}?`;
 
     Object.keys(urlOptions).forEach((key) => {
+      // todo: find a better workaround
+      if (key !== 'apiKey' && key !== 'sources') return;
+
       url += `${key}=${urlOptions[key]}&`;
     });
 
     return url.slice(0, -1);
   }
 
-  load(method, endpoint, callback, options = {}) {
+  load(method: 'GET', endpoint: NewsApiEndpoint, callback: CallbackFromView, options: NewsApiOptions = {}) {
     fetch(this.makeUrl(options, endpoint), { method })
       .then(this.errorHandler)
       .then((res) => res.json())
