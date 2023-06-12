c5d2448583c7b010a5a5ef20607ace707c36819c
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-06-12 23:07:44 +0300

    refactor: create Article interface

--- migration-to-ts/src/components/types/index.ts
+++ migration-to-ts/src/components/types/index.ts
@@ -1,1 +1,13 @@
 export type CallbackFromView = (data?: JSON) => void;
+
+export interface Article {
+  source: {
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+}
